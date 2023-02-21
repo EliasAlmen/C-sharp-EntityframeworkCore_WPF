bd5d1f51581db9ef3e4d5f202249fc0fa0f78a9b
EliasAlmen <almen.elias@gmail.com>
2023-02-21 10:32:11 +0100

    Add project files.

---
+++ EntityframeworkCore_WPF/Models/Entities/CategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityframeworkCore_WPF.Models.Entities
+{
+    internal class CategoryEntity
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<ProductEntity>
+            Products
+        { get; private set; } =
+            new ObservableCollection<ProductEntity>();
+    }
+}
